049d04bd5cfa5925799342592141c91036355870
generic-state-5961 <ericjchristine@gmail.com>
2020-08-02 00:23:00 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,10 @@
 # Write your code here
 
-magic_exit_number = 0
 
 count = 10 
 while count >=1 do
    puts count
-   count = count -1 
+   count -= 1
 end
-puts "Happy New Year"
+puts "Happy New Year!"
 
